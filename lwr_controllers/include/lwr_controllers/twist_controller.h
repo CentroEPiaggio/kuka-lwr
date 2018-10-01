2d62d8ad2f9554b3e770e709881893501b58a3c8
George Jose Pollayil <gpollayil@gmail.com>
2018-10-01 13:43:58 +0200

    Coded the fucntions init, starting and command

--- lwr_controllers/include/lwr_controllers/twist_controller.h
+++ lwr_controllers/include/lwr_controllers/twist_controller.h
@@ -3,12 +3,13 @@
 
 #include <vector>
 #include <boost/scoped_ptr.hpp>
-#include "KinematicChainControllerBase.h"
 #include <control_toolbox/pid.h>
+#include <geometry_msgs/Twist.h>
+#include "KinematicChainControllerBase.h"
 
 namespace lwr_controllers {
 
-  class TwistController : public controller_interface::KinematicChainControllerBase<hardware_interface::PositionJointInterface> {
+  class TwistController : public controller_interface::KinematicChainControllerBase<hardware_interface::EffortJointInterface> {
 
   public:
     // Default Constructor
@@ -18,10 +19,10 @@ namespace lwr_controllers {
     ~TwistController();
 
     // Controller functions
-    bool init(hardware_interface::PositionJointInterface *robot, ros::NodeHandle &n);
+    bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
 		void starting(const ros::Time& time);
 		void update(const ros::Time& time, const ros::Duration& period);
-		void command(const lwr_controllers::PoseRPY::ConstPtr &msg);
+		void command(const geometry_msgs::Twist::ConstPtr &msg);
 
     // Measured and commanded twists
     KDL::Twist twist_meas_, twist_des_;
@@ -32,17 +33,16 @@ namespace lwr_controllers {
     ros::Time last_time_;             // Used to compute dt
 
     // KDL Variables
-    KDL::Wrenches wrench_;            // Output of the PID
-    KDL::JntArray torques_;           // Joint torques given to effort interf.
     KDL::JntArrayVel joint_states_;   // Joint state containing q and q_dot
+    KDL::Wrenches wrenches_;          // Output of the PID
+    KDL::JntArray torques_;           // Joint torques given to effort interf.
     KDL::Jacobian jacobian_;          // Robot jacobian
 
     // Solvers (twist and jacobian solvers)
     boost::scoped_ptr<KDL::ChainFkSolverVel> jnt_to_twist_solver_;
     boost::scoped_ptr<KDL::ChainJntToJacSolver> jnt_to_jac_solver_;
 
-
-    // PID controllers (wrench_ = PID(s) * (twist_des_ - twist_meas_))
+    // PID controllers (wrenches_ = PID(s) * (twist_des_ - twist_meas_))
     std::vector<control_toolbox::Pid> pid_controller_;
 
   };
--- lwr_controllers/src/twist_controller.cpp
+++ lwr_controllers/src/twist_controller.cpp
@@ -1,4 +1,6 @@
 #include <lwr_controllers/twist_controller.h>
+#include <pluginlib/class_list_macros.h>
+#include "kdl/chainfksolvervel_recursive.hpp"
 
 namespace lwr_controllers {
 
@@ -13,25 +15,75 @@ namespace lwr_controllers {
   }
 
   // Initializing Function
-  bool TwistController::init(hardware_interface::PositionJointInterface *robot,
+  bool TwistController::init(hardware_interface::EffortJointInterface *robot,
     ros::NodeHandle &n){
+    // Calling the Initializing function of KinematicChainControllerBase
+    if(!(KinematicChainControllerBase<hardware_interface::EffortJointInterface>::init(robot, n))){
+        ROS_ERROR("Couldn't initilize TwistController.");
+        return false;
+    }
 
+    // Resetting the solvers
+    jnt_to_twist_solver_.reset(new KDL::ChainFkSolverVel_recursive(kdl_chain_));
+    jnt_to_jac_solver_.reset(new KDL::ChainJntToJacSolver(kdl_chain_));
+
+    // Resizing vectors and jacobian
+    joint_states_.resize(kdl_chain_.getNrOfJoints());
+    wrenches_.resize(kdl_chain_.getNrOfJoints());
+    torques_.resize(kdl_chain_.getNrOfJoints());
+    jacobian_.resize(kdl_chain_.getNrOfJoints());
+
+    // Setting to zero the current effort
+    torques_.data.setZero();
+
+    // Create the PID controllers for following twist's x, y, z, r, p and y
+    control_toolbox::Pid pid;
+    if(!pid.init(ros::NodeHandle(n, "pid_trans"))){
+      ROS_ERROR("Couldn't find PID translation gains.");
+      return false;
+    }
+    for(unsigned int i = 0; i < 3; i++) pid_controller_.push_back(pid);
+
+    if(!pid.init(ros::NodeHandle(n, "pid_rot"))){
+      ROS_ERROR("Couldn't find PID rotation gains.");
+      return false;
+    }
+    for(unsigned int i = 0; i < 3; i++) pid_controller_.push_back(pid);
+
+    // Initializing subscriber to command topic
+    sub_command_ = n.subscribe<geometry_msgs::Twist>("command", 1,
+      &TwistController::command, this);
+
+    return true;
   }
 
   // Starting Function
   void TwistController::starting(const ros::Time& time){
+    // Resetting PID controllers
+    for(unsigned int i = 0; i < 6; i++) pid_controller_[i].reset();
 
+    // Getting previous time
+    last_time_ = time;
+
+    // Setting desired twist to zero
+    twist_des_ = KDL::Twist::Zero();
   }
 
   // Updating Function
   void TwistController::update(const ros::Time& time,
     const ros::Duration& period){
-
+    // Getting current time resolution
   }
 
   // Command Topic Callback Function
-  void TwistController::command(const lwr_controllers::PoseRPY::ConstPtr &msg){
-
+  void TwistController::command(const geometry_msgs::Twist::ConstPtr &msg){
+    // Converting and saving msg to twist_des_
+    twist_des_.vel(0) = msg->linear.x;
+    twist_des_.vel(1) = msg->linear.y;
+    twist_des_.vel(2) = msg->linear.z;
+    twist_des_.rot(0) = msg->angular.x;
+    twist_des_.rot(1) = msg->angular.y;
+    twist_des_.rot(2) = msg->angular.z;
   }
 
 }
